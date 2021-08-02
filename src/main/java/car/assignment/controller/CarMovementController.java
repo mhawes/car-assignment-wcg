ca3c671da1559289dc1fb5ecb4143f5ebfcd630f
Martin Hawes-Lambourne <hawesmartin@googlemail.com>
2021-08-02 12:55:28 +0100

    integration tests for range validation on coordinates

--- src/main/java/car/assignment/controller/CarMovementController.java
+++ src/main/java/car/assignment/controller/CarMovementController.java
@@ -1,7 +1,7 @@
 package car.assignment.controller;
 
 import car.assignment.dto.NewPositionRequest;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +10,8 @@ import java.awt.*;
 @RestController
 public class CarMovementController {
 
-    @GetMapping("/car/new/position")
-    public Point getNewPosition(@RequestBody NewPositionRequest newPositionRequest) {
+    @PostMapping("/car/new/position")
+    public Point newPosition(@RequestBody NewPositionRequest newPositionRequest) {
         return null;
     }
-
 }
--- src/test/java/car/assignment/integration/CarMovementTest.java
+++ src/test/java/car/assignment/integration/CarMovementTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static car.assignment.dto.MovementCommand.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,6 +33,7 @@ public class CarMovementTest {
 
         ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         Coordinate responseCoordinate = response.getBody();
         assertThat(responseCoordinate).isNotNull();
         assertThat(responseCoordinate.getX()).isEqualTo(7);
@@ -50,6 +53,7 @@ public class CarMovementTest {
 
         ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         Coordinate responseCoordinate = response.getBody();
         assertThat(responseCoordinate).isNotNull();
         assertThat(responseCoordinate.getX()).isEqualTo(6);
@@ -69,9 +73,81 @@ public class CarMovementTest {
 
         ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
 
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         Coordinate responseCoordinate = response.getBody();
         assertThat(responseCoordinate).isNotNull();
         assertThat(responseCoordinate.getX()).isEqualTo(4);
         assertThat(responseCoordinate.getY()).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldReturnBadRequestDueToMissingMandatoryFields() {
+        final NewPositionRequest request = NewPositionRequest.builder()
+                .initialCoordinate(Coordinate.builder().build())
+                .build();
+
+        ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void shouldReturnOkWithValidCoordinateRangeMax() {
+        final NewPositionRequest request = NewPositionRequest.builder()
+                .initialCoordinate(Coordinate.builder()
+                        .x(15)
+                        .y(15)
+                        .build())
+                .movementCommands(Collections.emptyList())
+                .build();
+
+        ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldReturnBadRequestDueToInvalidCoordinateRangeMax() {
+        final NewPositionRequest request = NewPositionRequest.builder()
+                .initialCoordinate(Coordinate.builder()
+                        .x(16)
+                        .y(16)
+                        .build())
+                .movementCommands(Collections.emptyList())
+                .build();
+
+        ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void shouldReturnBadRequestDueToInvalidCoordinateRangeMin() {
+        final NewPositionRequest request = NewPositionRequest.builder()
+                .initialCoordinate(Coordinate.builder()
+                        .x(0)
+                        .y(0)
+                        .build())
+                .movementCommands(Collections.emptyList())
+                .build();
+
+        ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void shouldReturnOkWithValidCoordinateRangeMin() {
+        final NewPositionRequest request = NewPositionRequest.builder()
+                .initialCoordinate(Coordinate.builder()
+                        .x(1)
+                        .y(1)
+                        .build())
+                .movementCommands(Collections.emptyList())
+                .build();
+
+        ResponseEntity<Coordinate> response = template.postForEntity("/car/new/position", request, Coordinate.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
